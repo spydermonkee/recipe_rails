9f79057925c89c37487b8ac95f2dffc397f81a81
Spyder Monkee McHenry <monkee.spyder@gmail.com>
2014-04-03 17:20:36 -0700

    working MVC for recipes, styles working better, etc

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,4 +10,38 @@ class RecipesController < ApplicationController
     render("/recipes/show.html.erb")
   end
 
+  def new
+    @recipe = Recipe.new
+    render "/recipes/new.html.erb"
+  end
+
+  def create
+    @recipe = Recipe.new(params[:recipe])
+    if @recipe.save
+      redirect_to "/recipes"
+    else
+      render "recipes/new"
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    render "/recipes/update.html.erb"
+  end
+
+  def update
+    @recipe = Recipe.find params[:id]
+    @recipe.update params[:recipe]
+    if @recipe.save
+      redirect_to('/recipes')
+    else
+      render('/recipes/update.html.erb')
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to '/recipes'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 RecipeCentral::Application.routes.draw do
 
 match("/recipes", { via: :get, to: 'recipes#index'})
+match("/recipes/new", {via: :get, to: 'recipes#new'})
+match("/recipes/new", {via: :post, to: 'recipes#create'})
 match("/recipes/:id", {via: :get, to: 'recipes#show'})
+match("/recipes/:id", {via: :delete, to: 'recipes#destroy'})
+match("/recipes/:id/edit", {via: :get, to: 'recipes#edit'})
+match("/recipes/:id", {via: [:patch, :post], to: 'recipes#update'})
+
+
 
 end
