436aff9c1868c5e05d70222a3f4a89cea67196d9
Spyder Monkee McHenry <monkee.spyder@gmail.com>
2014-04-04 17:19:37 -0700

    adds tags, refactors and messes some stuff up, good luck!

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,40 +2,38 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
-    render("/recipes/index.html.erb")
   end
 
   def show
     @recipe = Recipe.find(params[:id])
-    render("/recipes/show.html.erb")
   end
 
   def new
     @recipe = Recipe.new
-    render "/recipes/new.html.erb"
   end
 
   def create
     @recipe = Recipe.new(params[:recipe])
     if @recipe.save
-      redirect_to "/recipes"
+      flash[:notice] = "Your Recipe was created."
+      redirect_to recipes_path
     else
-      render "recipes/new"
+      render "new"
     end
   end
 
   def edit
     @recipe = Recipe.find(params[:id])
-    render "/recipes/update.html.erb"
   end
 
   def update
     @recipe = Recipe.find params[:id]
     @recipe.update params[:recipe]
     if @recipe.save
-      redirect_to('/recipes')
+      flash[:notice] = "Your Recipe was updated"
+      redirect_to recipe_path @recipe
     else
-      render('/recipes/update.html.erb')
+      render "edit"
     end
   end
 
